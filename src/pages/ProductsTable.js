8538164daaf7c1c584c666dd36433edc5bdd39a4
gratiela-com2go <gratiela@com2go.com>
2023-01-13 13:06:01 +0200

    extracted the sorting logic into a custom hook and added sorting to the prices columns

--- src/pages/ProductsTable.js
+++ src/pages/ProductsTable.js
@@ -12,7 +12,7 @@ const data = [
     {"code":"HD1130",
     "description":"0007 KYLINDRIKO MAXILARI MESIS",
     "availableQuantity": 5,
-    "expiryDate": "29/07/2023",
+    "expiryDate": "25/07/2023",
     "barcode":"5204914362857",
     "vendorCode":"V1216",
     "vendorDescription":"GNC CHRISTOU TRADING LTD",
@@ -34,22 +34,22 @@ const data = [
     "newRetailPrice":25.11},
     {"code":"HD21514","description":"0014/M MAXILARI YPNOU MEMORY",
     "availableQuantity": 9,
-    "expiryDate": "29/07/2023","barcode":"","vendorCode":"V1216","vendorDescription":"GNC CHRISTOU TRADING LTD","importerCode":"V1216","importerDescription":"GNC CHRISTOU TRADING LTD","ministryCode":"","storageConditions":"D","status":"Active","inActivationDate":"","categoryCode":"ORTHOPE","categoryDescription":"ORTHOPEDICS","lastPurchDate":"30/12/2022","dateOpened":"13/11/2018","wholeSalePrice":48.72,"retailPrice":80.0,"vatPercent":19.0,"backToStock":"","newWholeSalePrice":48.72,"newRetailPrice":67.23},
+    "expiryDate": "29/06/2023","barcode":"","vendorCode":"V1216","vendorDescription":"GNC CHRISTOU TRADING LTD","importerCode":"V1216","importerDescription":"GNC CHRISTOU TRADING LTD","ministryCode":"","storageConditions":"D","status":"Active","inActivationDate":"","categoryCode":"ORTHOPE","categoryDescription":"ORTHOPEDICS","lastPurchDate":"30/12/2022","dateOpened":"13/11/2018","wholeSalePrice":48.72,"retailPrice":80.0,"vatPercent":19.0,"backToStock":"","newWholeSalePrice":48.72,"newRetailPrice":67.23},
     {"code":"HD21136","description":"0015/L ANATOMIKO MAXILARI MEMORY",
     "availableQuantity": 2,
-    "expiryDate": "29/07/2023","barcode":"5204914336476","vendorCode":"V1216","vendorDescription":"GNC CHRISTOU TRADING LTD","importerCode":"V1216","importerDescription":"GNC CHRISTOU TRADING LTD","ministryCode":"","storageConditions":"D","status":"Active","inActivationDate":"","categoryCode":"ORTHOPE","categoryDescription":"ORTHOPEDICS","lastPurchDate":"09/01/2023","dateOpened":"13/11/2018","wholeSalePrice":52.67,"retailPrice":86.49,"vatPercent":19.0,"backToStock":"","newWholeSalePrice":52.67,"newRetailPrice":72.68},
+    "expiryDate": "29/05/2023","barcode":"5204914336476","vendorCode":"V1216","vendorDescription":"GNC CHRISTOU TRADING LTD","importerCode":"V1216","importerDescription":"GNC CHRISTOU TRADING LTD","ministryCode":"","storageConditions":"D","status":"Active","inActivationDate":"","categoryCode":"ORTHOPE","categoryDescription":"ORTHOPEDICS","lastPurchDate":"09/01/2023","dateOpened":"13/11/2018","wholeSalePrice":52.67,"retailPrice":86.49,"vatPercent":19.0,"backToStock":"","newWholeSalePrice":52.67,"newRetailPrice":72.68},
     {"code":"P04710","description":"0022/L ANA EPIGONATIDA NE ME SPEIROEIDI",
     "availableQuantity": 6,
-    "expiryDate": "29/07/2023","barcode":"","vendorCode":"V1216","vendorDescription":"GNC CHRISTOU TRADING LTD","importerCode":"V1216","importerDescription":"GNC CHRISTOU TRADING LTD","ministryCode":"","storageConditions":"D","status":"Active","inActivationDate":"","categoryCode":"ORTHOPE","categoryDescription":"ORTHOPEDICS","lastPurchDate":"20/12/2022","dateOpened":"20/12/2022","wholeSalePrice":45.0,"retailPrice":65.2,"vatPercent":5.0,"backToStock":"","newWholeSalePrice":45.0,"newRetailPrice":62.1},
+    "expiryDate": "19/07/2023","barcode":"","vendorCode":"V1216","vendorDescription":"GNC CHRISTOU TRADING LTD","importerCode":"V1216","importerDescription":"GNC CHRISTOU TRADING LTD","ministryCode":"","storageConditions":"D","status":"Active","inActivationDate":"","categoryCode":"ORTHOPE","categoryDescription":"ORTHOPEDICS","lastPurchDate":"20/12/2022","dateOpened":"20/12/2022","wholeSalePrice":45.0,"retailPrice":65.2,"vatPercent":5.0,"backToStock":"","newWholeSalePrice":45.0,"newRetailPrice":62.1},
     {"code":"P04463","description":"0023/L EPIGONATIDA ME METALLIKA STIRIGMATA",
     "availableQuantity": 8,
-    "expiryDate": "29/07/2023","barcode":"","vendorCode":"V1216","vendorDescription":"GNC CHRISTOU TRADING LTD","importerCode":"V1216","importerDescription":"GNC CHRISTOU TRADING LTD","ministryCode":"","storageConditions":"D","status":"Active","inActivationDate":"","categoryCode":"ORTHOPE","categoryDescription":"ORTHOPEDICS","lastPurchDate":"27/10/2022","dateOpened":"27/10/2022","wholeSalePrice":44.0,"retailPrice":63.75,"vatPercent":5.0,"backToStock":"","newWholeSalePrice":44.0,"newRetailPrice":60.71},
+    "expiryDate": "04/07/2025","barcode":"","vendorCode":"V1216","vendorDescription":"GNC CHRISTOU TRADING LTD","importerCode":"V1216","importerDescription":"GNC CHRISTOU TRADING LTD","ministryCode":"","storageConditions":"D","status":"Active","inActivationDate":"","categoryCode":"ORTHOPE","categoryDescription":"ORTHOPEDICS","lastPurchDate":"27/10/2022","dateOpened":"27/10/2022","wholeSalePrice":44.0,"retailPrice":63.75,"vatPercent":5.0,"backToStock":"","newWholeSalePrice":44.0,"newRetailPrice":60.71},
     {"code":"P04519","description":"0023/XL EPIGONATIDA ME METALLIKA STIRIGMATA",
     "availableQuantity": 20,
-    "expiryDate": "29/07/2023","barcode":"","vendorCode":"V1216","vendorDescription":"GNC CHRISTOU TRADING LTD","importerCode":"V1216","importerDescription":"GNC CHRISTOU TRADING LTD","ministryCode":"","storageConditions":"D","status":"Active","inActivationDate":"","categoryCode":"ORTHOPE","categoryDescription":"ORTHOPEDICS","lastPurchDate":"30/12/2022","dateOpened":"09/11/2022","wholeSalePrice":47.0,"retailPrice":68.0,"vatPercent":5.0,"backToStock":"","newWholeSalePrice":47.0,"newRetailPrice":64.76},
+    "expiryDate": "05/07/2024","barcode":"","vendorCode":"V1216","vendorDescription":"GNC CHRISTOU TRADING LTD","importerCode":"V1216","importerDescription":"GNC CHRISTOU TRADING LTD","ministryCode":"","storageConditions":"D","status":"Active","inActivationDate":"","categoryCode":"ORTHOPE","categoryDescription":"ORTHOPEDICS","lastPurchDate":"30/12/2022","dateOpened":"09/11/2022","wholeSalePrice":47.0,"retailPrice":68.0,"vatPercent":5.0,"backToStock":"","newWholeSalePrice":47.0,"newRetailPrice":64.76},
     {"code":"P04730","description":"0029 EPIGONATIDIKI DESTRA",
     "availableQuantity": 10,
-    "expiryDate": "29/07/2023","barcode":"","vendorCode":"V1216","vendorDescription":"GNC CHRISTOU TRADING LTD","importerCode":"V1216","importerDescription":"GNC CHRISTOU TRADING LTD","ministryCode":"","storageConditions":"D","status":"Active","inActivationDate":"","categoryCode":"ORTHOPE","categoryDescription":"ORTHOPEDICS","lastPurchDate":"20/12/2022","dateOpened":"02/01/2023","wholeSalePrice":12.0,"retailPrice":17.38,"vatPercent":5.0,"backToStock":"","newWholeSalePrice":12.0,"newRetailPrice":16.55}
+    "expiryDate": "29/03/2024","barcode":"","vendorCode":"V1216","vendorDescription":"GNC CHRISTOU TRADING LTD","importerCode":"V1216","importerDescription":"GNC CHRISTOU TRADING LTD","ministryCode":"","storageConditions":"D","status":"Active","inActivationDate":"","categoryCode":"ORTHOPE","categoryDescription":"ORTHOPEDICS","lastPurchDate":"20/12/2022","dateOpened":"02/01/2023","wholeSalePrice":12.0,"retailPrice":17.38,"vatPercent":5.0,"backToStock":"","newWholeSalePrice":12.0,"newRetailPrice":16.55}
 ];
 
     const config = [
@@ -73,11 +73,13 @@ const data = [
         },
         {
             label: "ΧΤ",
-            render: (product) => product.wholeSalePrice
+            render: (product) => product.wholeSalePrice,
+            sortValue: (product) => product.wholeSalePrice
         },
         {
             label: "ΛΤ",
-            render: (product) => product.retailPrice
+            render: (product) => product.retailPrice,
+            sortValue: (product) => product.retailPrice
         },
         {
             label: "Κωδ.Φ/Υ",
