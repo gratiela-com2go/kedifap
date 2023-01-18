ec6f5e80c1d8b3ad0854da659c168c39785905a8
gratiela-com2go <gratiela@com2go.com>
2023-01-18 15:34:27 +0200

    added contact page including Google Maps. Api key to be created and added to the code

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -1,7 +1,21 @@
+import ContactInfo from "../components/Contact/ContactInfo";
+import {  useLoadScript} from "@react-google-maps/api";
+import Map from "../components/Contact/Map";
+
+
 function Contact() {
+
+    const { isLoaded } = useLoadScript({
+        googleMapsApiKey: ""
+    });
+
     return (
-        <div>
-            <h1 className="text-2xl py-4">Contact</h1>
+        <div className="shadow-md rounded-md">
+            <h1 className="text-2xl p-4 bg-kedifapgreen-300 rounded-md ">Στοιχεία Επικοινωνίας</h1>
+            <div className="flex items-top py-5 px-4">
+                <ContactInfo />
+                {!isLoaded ? <div>Loading ...</div> : <Map /> }
+            </div>
         </div>
     );
 }
